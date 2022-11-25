7824d0df05cb7e9a70405241d921530a1f539691
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2022-11-25 10:27:52 +0600

    sneakers_shop_app_added

---
+++ lib/constanst.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xffebebeb);
+const Color darkTextColor = Color(0xff171717);
+const Color lightTextColor = Colors.white;
+const Color unSelectedtextColor = Color(0xffc9c9c9);
+const Color materialButtonColor = Color(0xfffa2f65);
